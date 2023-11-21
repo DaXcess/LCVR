6b07a1bd71419f948968b6aac311642f30b9fe27
DaXcess <daxcess@daxcess.io>
2023-11-21 22:31:49 +0100

    Main menu controls work, started working on HUD but it's a mess rn

--- Plugin.cs
+++ Plugin.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Management;
