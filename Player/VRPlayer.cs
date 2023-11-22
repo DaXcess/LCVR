7c77d67229f1f55b89e12e540d7e4ba2dc97fc9a
DaXcess <daxcess@daxcess.io>
2023-11-22 17:34:17 +0100

    Small changes, checking if this HUD approach works

--- Player/VRPlayer.cs
+++ Player/VRPlayer.cs
@@ -7,48 +7,45 @@ namespace LethalCompanyVR.Player
 {
     public class VRPlayer
     {
-        // Attempt to assemble an XR Rig using current Player game objects
+        /// <summary>
+        /// Initialize an XR Rig on the Player game object
+        /// </summary>
         public static void InitializeXRRig()
         {
             // Find the "Player" game object
             GameObject player = GameObject.Find("Player");
 
             // Check if the player object is found
-            if (player != null)
+            if (player == null)
             {
-                // Add XR components to the player object
-                var xrRig = player.GetComponent<XROrigin>();
-                if (xrRig == null)
-                {
-                    xrRig = player.AddComponent<XROrigin>();
-                }
-
-                xrRig.CameraFloorOffsetObject = player;
-                xrRig.Origin = player;
-                xrRig.Camera = CameraPatches.activeCamera;
-
-                // Add locomotion system and other components
-                var locomotionSystem = player.AddComponent<LocomotionSystem>();
-                var snapTurnProvider = player.AddComponent<ActionBasedSnapTurnProvider>();
-
-                // Making component work
-                snapTurnProvider.enabled = true;
-                snapTurnProvider.leftHandSnapTurnAction = new InputActionProperty(Actions.XR_RightHand_Thumbstick);
-                snapTurnProvider.enableTurnLeftRight = true;
-                snapTurnProvider.turnAmount = 25f;
-                snapTurnProvider.debounceTime = .05f;
-                snapTurnProvider.system = locomotionSystem;
-
-                locomotionSystem.enabled = true;
-                locomotionSystem.xrOrigin = xrRig;
-
-                xrRig.enabled = true;
-
-            }
-            else
-            {
+                Logger.LogError("Could not find player object");
                 return;
             }
+
+            // Add XR components to the player object
+            var xrRig = player.GetComponent<XROrigin>() ?? player.AddComponent<XROrigin>();
+            xrRig.CameraFloorOffsetObject = player;
+            xrRig.Origin = player;
+            xrRig.Camera = Plugin.MainCamera;
+
+            // Add locomotion system and other components
+            var locomotionSystem = player.AddComponent<LocomotionSystem>();
+            var snapTurnProvider = player.AddComponent<ActionBasedSnapTurnProvider>();
+
+            // Making component work
+            snapTurnProvider.enabled = true;
+            snapTurnProvider.leftHandSnapTurnAction = new InputActionProperty(Actions.XR_RightHand_Thumbstick);
+            snapTurnProvider.enableTurnLeftRight = true;
+            snapTurnProvider.turnAmount = 25f;
+            snapTurnProvider.debounceTime = .05f;
+            snapTurnProvider.system = locomotionSystem;
+
+            locomotionSystem.enabled = true;
+            locomotionSystem.xrOrigin = xrRig;
+
+            xrRig.enabled = true;
+
+            Logger.LogDebug("XR Rig has been created");
         }
     }
 }
