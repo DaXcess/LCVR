1c786e5c2a0a18dec85116ec031a75559546596e
DaXcess <daxcess@daxcess.io>
2024-01-22 20:57:39 +0100

    Fix spectator voting

--- Patches/HUDManagerPatches.cs
+++ Patches/HUDManagerPatches.cs
@@ -1,4 +1,9 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine.InputSystem;
 
 namespace LCVR.Patches
 {
@@ -20,6 +25,32 @@ namespace LCVR.Patches
             }
 
             return true;
+        }
+    }
+
+    [LCVRPatch(LCVRPatchTarget.Universal)]
+    [HarmonyPatch(typeof(HUDManager), "Update")]
+    internal static class HUDManagerLeaveEarlyPatches
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var startIndex = codes.FindIndex(instruction => instruction.opcode == OpCodes.Callvirt && (MethodInfo)instruction.operand == AccessTools.PropertyGetter(typeof(PlayerActions), "Movement")) - 2;
+
+            var labels = codes[startIndex].labels;
+            codes[startIndex++] = new(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(IngamePlayerSettings), nameof(IngamePlayerSettings.Instance)))
+            {
+                labels = labels
+            };
+
+            codes[startIndex++] = new(OpCodes.Ldfld, AccessTools.Field(typeof(IngamePlayerSettings), nameof(IngamePlayerSettings.playerInput)));
+            codes[startIndex++] = new(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(PlayerInput), nameof(PlayerInput.actions)));
+            codes[startIndex++] = new(OpCodes.Ldstr, "PingScan");
+            codes[startIndex++] = new(OpCodes.Ldc_I4_0);
+            codes[startIndex++] = new(OpCodes.Callvirt, AccessTools.Method(typeof(InputActionAsset), nameof(InputActionAsset.FindAction), [typeof(string), typeof(bool)]));
+            codes[startIndex++] = new(OpCodes.Callvirt, AccessTools.Method(typeof(InputAction), nameof(InputAction.IsPressed)));
+
+            return codes.AsEnumerable();
         }
     }
 }
