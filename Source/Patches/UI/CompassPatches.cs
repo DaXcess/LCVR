578b0c647f9d06400c07c3d61cf88b3e0ae7bcdc
DaXcess <daxcess@daxcess.io>
2025-06-26 11:57:40 +0200

    Final 1.4.0 commit maybe?

--- Source/Patches/UI/CompassPatches.cs
+++ Source/Patches/UI/CompassPatches.cs
@@ -30,13 +30,13 @@ internal static class CompassPatches
         
         static float GetCurrentRotation()
         {
-            if (VRSession.Instance is not {} session)
+            if (VRSession.Instance is not { LocalPlayer: {} player } session)
                 return GameNetworkManager.Instance.localPlayerController.transform.eulerAngles.y / 360;
 
             if (session.HUD.isHandUiDisabled)
                 return VRSession.Instance.MainCamera.transform.eulerAngles.y / 360;
                 
-            var origin = session.LocalPlayer.PrimaryController.InteractOrigin;
+            var origin = player.PrimaryController.InteractOrigin;
             var fwd = Vector3.Dot(origin.forward, Vector3.up) > 0.7f ? -origin.right : origin.forward;
             var normalized = new Vector3(fwd.x, 0, fwd.z).normalized;
             var angle = Mathf.Atan2(normalized.x, normalized.z) * Mathf.Rad2Deg;
